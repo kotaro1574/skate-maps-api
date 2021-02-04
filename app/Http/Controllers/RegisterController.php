2e957a0ad0d916c15250410d27709068986e8888
kotaro1574 <shake1574@gmail.com>
2021-02-04 15:53:58 +0900

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function post(Request $request)
+    {
+        $now = Carbon::now();
+        $hashed_password = Hash::make($request->password);
+        $param = [
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => $hashed_password,
+            "address" => $request->address,
+            "created_at" => $now,
+            "updated_at" => $now,
+        ];
+        DB::table('users')->insert($param);
+        return response()->json([
+            'message' => 'User created successfully',
+            'data' => $param
+        ], 200);
+    }
+}
